24e5dbc5bc1363a7155c11ef28510a2d77de35f9
Thirumaran-M <maranletters@gmail.com>
2024-02-27 01:13:03 +0530

    Updated CSS and JS

--- script.js
+++ script.js
@@ -1,70 +1,50 @@
-const text = document.createElement("p");
+const text = document.createElement("h1");
 text.className= "View";
 
 document.body.append(text);
-F10();
-function F10(){ 
-    setTimeout(()=>{
-        text.innerHTML = `10`;
-    }, 1000);
-    F9();
-    function F9(){ 
-        setTimeout(()=>{
-            text.innerHTML = `9`;
-        }, 2000);
-        F8();
-        function F8(){ 
-            setTimeout(()=>{
-                text.innerHTML = `8`;
-            }, 3000);
-            F7();
-            function F7(){ 
-                setTimeout(()=>{
-                    text.innerHTML = `7`;
-                }, 4000);
-                F6();
-                function F6(){ 
-                    setTimeout(()=>{
-                        text.innerHTML = `6`;
-                    }, 5000);
-                    F5();
-                    function F5(){ 
-                        setTimeout(()=>{
-                            text.innerHTML = `5`;
-                        }, 6000);
-                        F4();
-                        function F4(){ 
-                            setTimeout(()=>{
-                                text.innerHTML = `4`;
-                            }, 7000);
-                            F3();
-                            function F3(){ 
-                                setTimeout(()=>{
-                                    text.innerHTML = `3`;
-                                }, 8000);
-                                F2();
-                                function F2(){ 
-                                    setTimeout(()=>{
-                                        text.innerHTML = `2`;
-                                    }, 9000);
-                                    F1();
-                                    function F1(){ 
-                                        setTimeout(()=>{
-                                            text.innerHTML = `1`;
-                                        }, 10000);
-                                        F0();
-                                        function F0(){ 
-                                            setTimeout(()=>{
-                                                text.innerHTML = `Happy Independence Day!`;
-                                            }, 11000);
+
+const countdown = () => {
+    setTimeout(()=>{text.innerHTML = `10`;}, 1000);
+    const countdown1 = () => {
+        setTimeout(()=>{text.innerHTML = `9`;}, 2000);
+        const countdown2 = () => {
+            setTimeout(()=>{text.innerHTML = `8`;}, 3000);
+            const countdown3 = () => {
+                setTimeout(()=>{text.innerHTML = `7`;}, 4000);
+                const countdown4 = () => {
+                    setTimeout(()=>{text.innerHTML = `6`;}, 5000);
+                    const countdown5 = () => {
+                        setTimeout(()=>{text.innerHTML = `5`;}, 6000);
+                        const countdown6 = () => {
+                            setTimeout(()=>{text.innerHTML = `4`;}, 7000);
+                            const countdown7 = () => {
+                                setTimeout(()=>{text.innerHTML = `3`;}, 8000);
+                                const countdown8 = () => {
+                                    setTimeout(()=>{text.innerHTML = `2`;}, 9000);
+                                    const countdown9 = () => {
+                                        setTimeout(()=>{text.innerHTML = `1`;}, 10000);
+                                        const Independence = () => {
+                                            setTimeout(()=>{text.innerHTML = `Happy Independence Day!`;}, 11000);
+                                            
                                         }
+                                        Independence();
                                     }
+                                    countdown9();
                                 }
+                                countdown8();
                             }
+                            countdown7();
                         }
+                        countdown6();
                     }
+                    countdown5();
                 }
+                countdown4();
             }
+            countdown3();
         }
+        countdown2();
     }    
+    countdown1();
 }
+countdown();
